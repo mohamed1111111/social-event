664b01800cd92fa389f4b9cdc239654b6e25e4e0
MohameddIsmail <esmailmohamed185@yahoo.com>
2021-01-10 20:19:59 +0000

    Update DownloadCsv.php

--- app/Console/Commands/DownloadCsv.php
+++ app/Console/Commands/DownloadCsv.php
@@ -39,30 +39,49 @@ class DownloadCsv extends Command
      */
     public function handle()
     {
-        $from =  '2021-01-01';
-        $to = '2021-12-31';
-        $name = $this->ask('name of  generated file?  like:  task.csv');
+        $from = Carbon::now()->format('Y-m-d');
+        $to = Carbon::now()->addYear()->format('Y-m-d');
+        $name = $this->ask('name of  generated file?  like:  task.csv or we add a default name') ?? 'period.csv';
         $period = CarbonPeriod::create($from, '1 month', $to);
-        $months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
-        $x=0;
-        $array=array();
-        foreach ($period  as $key => $value){
+        $monthName = $this->getTheNameOfTheMonth();
+        $data = array();
+        foreach ($period as $key => $value) {
 
-            $array[$key][0]=$value->format('y-m');
-            $array[$key][1]=(new Carbon("first Monday of.$months[$x]."))->format('d-m-y');
-            $array[$key][2]=($value->nthOfMonth(3, Carbon::THURSDAY ))->format('d-m-y');
-            $array[$key][3]=(new Carbon("last FRIDAY of.$months[$x]."))->format('d-m-y');
-            if($value->format("y-m") !=("12-21")){
-                $x++;
-            }
+            $data[$key][0] = $value->format('y-m');
+            $data[$key][1] = (new Carbon("first Monday of.$monthName[$key]."))->format('d-m-y');
+            $data[$key][2] = ($value->nthOfMonth(3, Carbon::THURSDAY))->format('d-m-y');
+            $data[$key][3] = (new Carbon("last FRIDAY of.$monthName[$key]."))->format('d-m-y');
         }
-        array_unshift($array,['Month','Brunch & Catchup','Thirsty Thursday','Friday Fry-up']);
+        array_unshift($data, ['Month', 'Brunch & Catchup', 'Thirsty Thursday', 'Friday Fry-up']);
 
         $fp = fopen($name, 'w');
-        foreach ($array as $key =>  $fields) {
+        foreach ($data as $key => $fields) {
             fputcsv($fp, $fields);
         }
         fclose($fp);
         echo "Good job, The  file is generated in project path";
     }
+
+    /**
+     * @return string[]
+     */
+    public function getTheNameOfTheMonth(): array
+    {
+        return [
+            0 => 'january',
+            1 => 'january',
+            2 => 'february',
+            3 => 'march',
+            4 => 'april',
+            5 => 'may',
+            6 => 'june',
+            7 => 'july',
+            8 => 'august',
+            9 => 'september',
+            10 => 'october',
+            11 => 'november',
+            12 => 'december',
+        ];
+    }
+ 
 }
